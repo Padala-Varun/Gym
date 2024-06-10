b1b5c431b512bc97e896030d3aa35caf601ef038
Padala-Varun <varun2004varun2004@gmail.com>
2024-06-10 21:08:46 +0530

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
     setLoading(true);
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/send/mail",
+        "https://gym-1-wli3.onrender.com/send/mail",
         {
           name,
           email,
